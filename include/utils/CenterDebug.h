93d7ac18701a34acbadea41d9d1f6a940635c1c1
lzx-center <z870180527@qq.com>
2021-06-07 19:59:15 +0800

    change debug

--- include/utils/CenterDebug.h
+++ include/utils/CenterDebug.h
@@ -16,3 +16,18 @@
         std::cout<<"\033[0m"; \
     }\
 } while(false);
+
+namespace color {
+    const std::string red = "\033[31m";
+    const std::string green = "\033[32m";
+    const std::string yellow = "\033[33m";
+    const std::string blue = "\033[34m";
+}
+
+#define DEBUG_WITH_COLOR(X, COLOR) do { \
+    if (CENTER_DEBUG) {\
+        std::cout<< COLOR; \
+        X;                   \
+        std::cout<<"\033[0m"; \
+    }\
+} while(false);

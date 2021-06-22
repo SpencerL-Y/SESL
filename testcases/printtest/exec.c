4b03a2d7e684bbb08b4c5a07cb87c7f652b054e4
Xie Li <lixie15@mails.ucas.ac.cn>
2021-06-22 11:22:25 +0800

    update

--- testcases/printtest/exec.c
+++ testcases/printtest/exec.c
@@ -1,31 +1,23 @@
 #include "smack.h"
 #include <stdlib.h>
 
-typedef struct {
+typedef struct test{
     int a;
     char b;
+    struct test* next;
 } test_type;
 
 int main(){
-    int num = 5;
-    //int j = 0;
-    //j = j + 1;
-    //int *i = malloc(num*sizeof(int));
-    //char *j = (char*)malloc(num);
+    int num = 9;
     int *j = (int*)malloc(num*sizeof(int));
-    //*(i+1) = 101;
-    //*(i+3) = 1111;
-    //*(2+i) = 123;
     *(j+1) = 1110;
-    int nnum = *(j+2) + (-1);
-    //int nnum = *(i+1) + (-1);
-    //*(i+1) = num;
-    //*(i+2) = 11;
-    int a[2][3];
-    a[1][2] = num;
-    test_type my;
-    my.b = 'a';
+    int nnum = *(j+1) + (-1);
+    // int a[2][3];
+    // a[1][2] = num;
+    // test_type my;
+    // my.b = 'a';
+    // test_type nextMy;
+    // my.next = &nextMy;
     //free(j);
-    // byte level
     
 }

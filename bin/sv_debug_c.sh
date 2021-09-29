ab04db8723fb57ae05699ac8b6554f4a8ab42213
Xie Li <lixie15@mails.ucas.ac.cn>
2021-09-29 17:31:40 +0800

    update

--- bin/sv_debug_c.sh
+++ bin/sv_debug_c.sh
@@ -7,5 +7,5 @@ if [ ! -f "/usr/local/bin/libz3.so" ];then
   sudo cp ../lib/libz3.so /usr/local/lib
 fi
 export LD_LIBRARY_PATH=/usr/local/lib:../lib:$LD_LIBRARY_PATH
-# sudo ldconfig
+sudo ldconfig
 cd ../build && sudo ninja install && smack ../testcases/svcomp/memsafety/$1.c -ll ../testcases/svcomp/memsafety/$1_IR.ll --bpl ../testcases/svcomp/memsafety/$1.bpl -t --sh-mem-leak --add-line-info

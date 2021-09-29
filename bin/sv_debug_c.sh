19b3b6e7839b045b383a065b6a9b814dfcb99650
Xie Li <lixie15@mails.ucas.ac.cn>
2021-09-29 17:15:52 +0800

    modify scripts in bin

--- bin/sv_debug_c.sh
+++ bin/sv_debug_c.sh
@@ -6,6 +6,6 @@
 # if [ ! -f "/usr/local/bin/libz3.so" ];then
 #   sudo cp ../lib/libz3.so /usr/local/lib
 # fi
-export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:../lib:$LD_LIBRARY_PATH
 # sudo ldconfig
 cd ../build && sudo ninja install && smack ../testcases/svcomp/memsafety/$1.c -ll ../testcases/svcomp/memsafety/$1_IR.ll --bpl ../testcases/svcomp/memsafety/$1.bpl -t --sh-mem-leak --add-line-info

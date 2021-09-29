9ad4a7fae95e4223de0baaad341f57eac7774b3f
Xie Li <lixie15@mails.ucas.ac.cn>
2021-09-29 17:25:10 +0800

    slight change

--- bin/sv_debug_c.sh
+++ bin/sv_debug_c.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-# if [ ! -f "/usr/local/bin/libslah.so" ];then
-#   sudo cp ../lib/libslah.so /usr/local/lib
-# fi
+!/bin/bash
+if [ ! -f "/usr/local/bin/libslah.so" ];then
+  sudo cp ../lib/libslah.so /usr/local/lib
+fi
 
-# if [ ! -f "/usr/local/bin/libz3.so" ];then
-#   sudo cp ../lib/libz3.so /usr/local/lib
-# fi
+if [ ! -f "/usr/local/bin/libz3.so" ];then
+  sudo cp ../lib/libz3.so /usr/local/lib
+fi
 export LD_LIBRARY_PATH=/usr/local/lib:../lib:$LD_LIBRARY_PATH
 # sudo ldconfig
 cd ../build && sudo ninja install && smack ../testcases/svcomp/memsafety/$1.c -ll ../testcases/svcomp/memsafety/$1_IR.ll --bpl ../testcases/svcomp/memsafety/$1.bpl -t --sh-mem-leak --add-line-info
--- bin/ter_build.sh
+++ bin/ter_build.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-# if [ ! -f "/usr/local/bin/libslah.so" ];then
-#   sudo mv ../lib/libslah.so /usr/local/lib
-# fi
+!/bin/bash
+if [ ! -f "/usr/local/bin/libslah.so" ];then
+  sudo mv ../lib/libslah.so /usr/local/lib
+fi
 
-# if [ ! -f "/usr/local/bin/libz3.so" ];then
-#   sudo mv ../lib/libz3.so /usr/local/lib
-# fi
+if [ ! -f "/usr/local/bin/libz3.so" ];then
+  sudo mv ../lib/libz3.so /usr/local/lib
+fi
 export LD_LIBRARY_PATH=/usr/local/lib:../lib:$LD_LIBRARY_PATH
 cd ../build && sudo ninja install && smack ../testcases/tercases/$1.c -ll ../testcases/tercases/$1_IR.ll --bpl ../testcases/tercases/$1.bpl -t

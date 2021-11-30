d871f8a0bb2e4bd7eed382bdf97e7dc43498ae43
--get <lixie19@ios.ac.cn>
2021-11-30 20:45:13 +0800

    update

--- lib/smack/SVNaming.cpp
+++ lib/smack/SVNaming.cpp
@@ -5,6 +5,6 @@ namespace smack
     const std::string SVNaming::SV_NONDET_CHAR = "__VERIFIER_nondet_char";
     const std::string SVNaming::SV_NONDET_INT  = "__VERIFIER_nondet_int";
     const std::string SVNaming::SV_NONDET_LONG  = "__VERIFIER_nondet_long";
-    const std::string SVNaming::SV_NONDET_ULONG = "__VERIFIER_nondet_long";
+    const std::string SVNaming::SV_NONDET_ULONG = "__VERIFIER_nondet_ulong";
     const std::string SVNaming::SV_NONDET_BOOL = "__VERIFIER_nondet_bool";
 } // namespace smack

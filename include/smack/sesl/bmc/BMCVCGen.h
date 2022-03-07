ca12cbcadc75cc12a4fdd239928109c1845cceb5
--get <lixie19@ios.ac.cn>
2022-03-07 11:22:02 +0800

    eat lunch

--- include/smack/sesl/bmc/BMCVCGen.h
+++ include/smack/sesl/bmc/BMCVCGen.h
@@ -11,6 +11,51 @@
 
 namespace smack
 {
+    class BlockNormalForm {
+        private:
+            int blockId;
+            int length;
+            int primeNum;
+            z3::context* z3Ctx;
+        public:
+            BlockNormalForm(z3::context& ctx, int blockId, int l, int primeNum){
+                this->z3Ctx = &ctx;
+                this->blockId = blockId;
+                this->length = l;
+                this->primeNum = primeNum;
+            }
+            int getLength(){return this->length;}
+            int getBlockId(){return this->blockId;}
+            int getPrimeNum(){return this->primeNum;}
+
+            std::vector<z3::expr> getBNFVars();
+            std::vector<z3::expr> getBNFAddrVars();
+            std::vector<std::pair<z3::expr, z3::expr>> getBNFEqualPairs();
+            z3::expr generateImplicitConstraint();
+            z3::expr generateInitialConfiguration();
+            
+            // TODObmc: return the used var in the bnf formula
+    };
+    typedef std::shared_ptr<BlockNormalForm> BNFPtr;
+
+    class RegionNormalForm {
+        private:
+            int maxRegionNum;
+            int primeNum;
+            z3::context* z3Ctx;
+            std::vector<BNFPtr> bnfList;
+        public:
+            // TODObmc: imple
+            RegionNormalForm(z3::context& ctx, int regionNum, int primeNum);
+            int getMaxRegionNum(){return this->maxRegionNum;}
+            int getPrimeNum(){return this->primeNum;}
+            std::vector<BNFPtr> getBnfList(){return this->bnfList;}
+            z3::expr generateImplicitConstraint();
+            std::vector<z3::expr> getRNFVars();
+    };
+    typedef std::shared_ptr<RegionNormalForm> RNFPtr;
+
+
     class BMCVCGen {
         private:
             z3::context z3Ctx;
@@ -36,48 +81,18 @@ namespace smack
             z3::expr generateATSTransitionRelation();
 
             // Stmt semantic encoding
-            z3::expr generateTrMalloc(int u);
-            z3::expr generateTrFree(int u);
-            z3::expr generateTrStore(int u);
-            z3::expr generateTrLoad(int u);
-            z3::expr generateTrUnchage(int u);
-            z3::expr generateTrAssert(int u);
-            z3::expr generateTrOtherProc(int u);
-            z3::expr generateTrCommonAssign(int u);
-            z3::expr generateTrOther(int u);
+            z3::expr generateTrMalloc(RNFPtr rnf);
+            z3::expr generateTrFree(RNFPtr rnf);
+            z3::expr generateTrStore(RNFPtr rnf);
+            z3::expr generateTrLoad(RNFPtr rnf);
+            z3::expr generateTrUnchage(RNFPtr rnf);
+            z3::expr generateTrAssert(RNFPtr rnf);
+            z3::expr generateTrOtherProc(RNFPtr rnf);
+            z3::expr generateTrCommonAssign(RNFPtr rnf);
+            z3::expr generateTrOther(RNFPtr rnf);
     };
 
-    class BlockNormalForm {
-        private:
-            int regId;
-            int length;
-            int primeNum;
-            z3::context* z3Ctx;
-        public:
-            BlockNormalForm(z3::context& ctx, int regId, int l, int primeNum){
-                this->z3Ctx = &ctx;
-                this->regId = regId;
-                this->length = l;
-                this->primeNum = primeNum;
-            }
-            int getLength(){return this->length;}
-            std::vector<z3::expr> getBNFVars();
-            // TODObmc: return the used var in the bnf formula
-    };
-    typedef std::shared_ptr<BlockNormalForm> BNFPtr;
-
-    class RegionNormalForm {
-        private:
-            int maxRegionNum;
-            int primeNum;
-            z3::context* z3Ctx;
-            std::vector<BNFPtr> bnfList;
-        public:
-            // TODObmc: imple
-            RegionNormalForm(z3::context& ctx, int regionNum, int primeNum);
-    };
-    typedef std::shared_ptr<RegionNormalForm> RNFPtr;
-
+    
 
 } // namespace smack
 
--- lib/smack/sesl/bmc/BMCVCGen.cpp
+++ lib/smack/sesl/bmc/BMCVCGen.cpp
@@ -2,52 +2,54 @@
 
 namespace smack
 {
-        z3::expr BMCVCGen::generateFeasibleVC(){
-
+    std::vector<z3::expr> BlockNormalForm::getBNFVars(){
+        std::list<std::string> varNames;
+        for(int i = 0; i < this->length; i++){
+            varNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*i));
+            varNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*i + 1));
+            varNames.push_back("pta_" + std::to_string(this->blockId) + "_" + std::to_string(2*i + 1));
+            varNames.push_back("ptd_" + std::to_string(this->blockId) + "_" + std::to_string(2*i + 1));
         }
-
-        z3::expr BMCVCGen::generateViolation(){
-
+        varNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*this->length));
+        varNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*this->length + 1));
+        std::vector<z3::expr> bnfVars;
+        for(std::string s : varNames){
+            z3::expr tempVar = this->z3Ctx->int_const(s.c_str());
+            bnfVars.push_back(tempVar);
         }
-        // Detailed violation situation encodings
-        z3::expr BMCVCGen::generateATSTransitionRelation(){
+        return bnfVars;
+    }
 
+    std::vector<z3::expr> BlockNormalForm::getBNFAddrVars(){
+        std::list<std::string> addrVarNames;
+        for(int i = 0; i < this->length; i++){
+            addrVarNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*i));
+            addrVarNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*i + 1));
+            addrVarNames.push_back("pta_" + std::to_string(this->blockId) + "_" + std::to_string(2*i + 1));
         }
-        // Stmt semantic encoding
-        z3::expr BMCVCGen::generateTrMalloc(int u){
-
+        addrVarNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*this->length));
+        addrVarNames.push_back("blka_" + std::to_string(this->blockId) + "_" + std::to_string(2*this->length + 1));
+        std::vector<z3::expr> bnfAddrVars;
+        for(std::string s : addrVarNames){
+            z3::expr tempVar = this->z3Ctx->int_const(s.c_str());
+            bnfAddrVars.push_back(tempVar);
         }
+        return bnfAddrVars;
+    }
 
-        z3::expr BMCVCGen::generateTrFree(int u){
+    z3::expr BlockNormalForm::generateInitialConfiguration(){
 
+    }
+
+    z3::expr BlockNormalForm::generateImplicitConstraint(){
+        z3::expr addrOrder = this->z3Ctx->bool_val(true);
+        std::vector<z3::expr> addrList = this->getBNFAddrVars();
+        for(int i = 0; i < addrList.size() - 1; i++){
+            z3::expr left = addrList[i];
+            z3::expr right = addrList[i + 1];
         }
+    }
 
-        z3::expr BMCVCGen::generateTrStore(int u){
 
-        }
-
-        z3::expr BMCVCGen::generateTrLoad(int u){
-
-        }
-
-        z3::expr BMCVCGen::generateTrUnchage(int u){
-
-        }
-
-        z3::expr BMCVCGen::generateTrAssert(int u){
-
-        }
-
-        z3::expr BMCVCGen::generateTrOtherProc(int u){
-
-        }
-
-        z3::expr BMCVCGen::generateTrCommonAssign(int u){
-
-        }
-
-        z3::expr BMCVCGen::generateTrOther(int u){
-
-        }
 
 } // namespace smack

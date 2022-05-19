3f725740702431d6518ce585a219416d816e396d
zhuyt <zhuyutian57@gmail.com>
2022-05-19 11:17:09 +0800

    separate ComSpen and z3 : find their pacakges in system

--- bin/sv_debug_c.sh
+++ bin/sv_debug_c.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [ ! -f "/usr/local/bin/libslah.so" ];then
-  sudo cp ../lib/libslah.so /usr/local/lib
-fi
-
-if [ ! -f "/usr/local/bin/libz3.so" ];then
-  sudo cp ../lib/libz3.so /usr/local/lib
-fi
 export LD_LIBRARY_PATH=/usr/local/lib:../lib:$LD_LIBRARY_PATH
 sudo ldconfig
 
